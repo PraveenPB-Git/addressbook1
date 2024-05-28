817ddff97194040d9bd96b1cbb93eaefe41a6cdc
PraveenPB-Git <153975322+PraveenPB-Git@users.noreply.github.com>
2024-05-28 20:25:10 +0530

    Updated Server Script file

--- server-config.sh
+++ server-config.sh
@@ -3,18 +3,19 @@
 sudo yum install git -y
 sudo yum install maven -y
 sudo yum install java-1.8.0-openjdk-devel -y 
-
-if [ -d "addressbook1" ]
-then
-   echo "repo is already cloned and exists"
+git clone https://github.com/PraveenPB-Git/addressbook1.git
+cd /home/ec2-user/addressbook1
+#if [ -d "addressbook1" ]
+#then
+#   echo "repo is already cloned and exists"
    #git config --global --add safe.directory /home/ec2-user/addressbook1
-   cd /home/ec2-user/addressbook1
-   git pull origin master
-else
-   git clone https://github.com/PraveenPB-Git/addressbook1.git
-   cd addressbook1
-   git checkout master
-fi
+   #cd /home/ec2-user/addressbook1
+   #git pull origin master
+#else
+   #git clone https://github.com/PraveenPB-Git/addressbook1.git
+   #cd addressbook1
+   #git checkout master
+#fi
 #export JAVA_HOME='/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.402.b06-1.amzn2.0.1.x86_64'
 #export PATH=$JAVA_HOME/bin:$PATH
 #source /etc/profile
