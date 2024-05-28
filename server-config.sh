039d9507f5e19baec3641fed471cb11d6c7f8b99
PraveenPB-Git <153975322+PraveenPB-Git@users.noreply.github.com>
2024-05-28 17:43:23 +0530

    Update server-config.sh

--- server-config.sh
+++ server-config.sh
@@ -8,16 +8,15 @@ if [ -d "addressbook1" ]
 then
    echo "repo is already cloned and exists"
    cd /home/ec2-user/addressbook1
-   git pull origin master
+#git pull origin master
 else
    git clone https://github.com/PraveenPB-Git/addressbook1.git
    cd addressbook1
-   git checkout master
+#git checkout master
 fi
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.402.b06-1.amzn2.0.1.x86_64
+export PATH=$JAVA_HOME/bin:$PATH
+source /etc/profile
 mvn package
 sudo yum install docker -y
 sudo systemctl start docker
-#export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.402.b06-1.amzn2.0.1.x86_64
-#export PATH=$JAVA_HOME/bin:$PATH
-#source /etc/profile
-#mvn package
