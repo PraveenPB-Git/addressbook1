c44a5ff37d2cfed926672aaeebfaac2581933903
PraveenPB-Git <153975322+PraveenPB-Git@users.noreply.github.com>
2024-05-28 15:32:13 +0530

    Updating the script file

--- server-config.sh
+++ server-config.sh
@@ -4,10 +4,10 @@ sudo yum install git -y
 sudo yum install maven -y
 sudo yum install java-1.8.0-openjdk-devel -y 
 
-if [ -d "addressbook" ]
+if [ -d "addressbook1" ]
 then
    echo "repo is already cloned and exists"
-   cd /home/ec2-user/addressbook
+   cd /home/ec2-user/addressbook1
    git pull origin master
 else
    git clone https://github.com/preethid/addressbook.git
